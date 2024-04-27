f0862bb3f475b2c2912ef4e1b95e62670d7cefde
Mila Severnini <milacsevernini@gmail.com>
2024-04-27 20:56:38 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { Header } from './Header';
+import { Footer } from './Footer';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
