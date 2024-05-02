89937c2590aa0b8bd381375acabb0523b200018f
Mila Severnini <milacsevernini@gmail.com>
2024-05-02 16:30:40 -0300

    Setup backend URL

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.send('Express is runnin g');
+  res.send('Express is running');
 });
 
 module.exports = router;
